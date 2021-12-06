7acdedc734f84e3e967b721eca8da1cbfc7181a9
Chris Riddell <chris.riddell@dreamboat.co>
2021-12-06 17:07:01 +1100

    tiny slideshow tweaks

--- slideshows/central_thesis/two/index.php
+++ slideshows/central_thesis/two/index.php
@@ -18,7 +18,7 @@ require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/economy/charts/gdp_per_capita_
 
 echo "<div class='slideshow_descriptive_text'>";
 list_start();
-    point_detailed_start('no', 10, 10,10,"If we don't manage to turn this <a href='https://www.investopedia.com/terms/p/per-capita-gdp.asp'>GDP per capita</a> into the target line marked...");
+    point_detailed_start('no', 10, 10,10,"If we don't manage to turn this <a href='https://www.investopedia.com/terms/p/per-capita-gdp.asp'>GDP per capita</a>trend around to look like what is above.");
     point_detailed_start('no', 10, 10,10,"Then there are all sorts of <a href='/knowledgebase/economy/downside_risks/'>risks</a> and <a href='https://dreamboat.com.au/knowledgebase/economy/other_real_world_implications/' target='_blank'>implications</a> that will almost certainly occur.");
     point_detailed_start('no', 10, 10,10,"Leading to the degradation of this <a href='https://en.wikipedia.org/wiki/Australia' target='_blank'>great nation</a>.");
 list_end();
