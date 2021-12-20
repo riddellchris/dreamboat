5254b458dfea37f9cf23063c32c190a4cc37315c
Chris Riddell <chris.riddell@dreamboat.co>
2021-12-20 13:35:18 +1100

    redirect uri maps to map for live server

--- map/apis/xero/redirect_uri.php
+++ map/apis/xero/redirect_uri.php
@@ -2,7 +2,7 @@
 if(!isset($_SESSION)){session_start();}
 
 if($_SERVER['SERVER_PORT'] != 8888){
-    $redirect_uri   = "https://dreamboat.com.au/maps/apis/xero/connect.php";
+    $redirect_uri   = "https://dreamboat.com.au/map/apis/xero/connect.php";
 }
 else{
     $redirect_uri   = "http://localhost:8888/map/apis/xero/connect.php";
