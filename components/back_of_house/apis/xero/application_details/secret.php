5de2ca2a2ed011f9c65eb780184d7ca90f9872c9
Chris Riddell <chris.riddell@dreamboat.co>
2021-12-23 15:00:28 +1100

    big insights progress through to report collection

--- components/back_of_house/apis/xero/application_details/secret.php
+++ components/back_of_house/apis/xero/application_details/secret.php
@@ -1,2 +1,8 @@
 <?php
+
+if($_SERVER['SERVER_PORT'] == '8888'){
+    $client_secret = "YPIiPqt_zz6p6flGPNvaz8vpUkJFMD8gLV5c0HxPlUKbmh3V";
+}
+else{
 $client_secret = "qMs4VcApsVbLsWkx-Ru8MnK-cnLaqdAX5AkPUdUeNb0qYtrS";
+}
