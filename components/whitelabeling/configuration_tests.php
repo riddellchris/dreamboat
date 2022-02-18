cc3e84b471edaaee737b8f11d5f72b4301cd7426
Chris Riddell <chris.riddell@dreamboat.co>
2022-02-18 14:17:45 +1100

    moving to whitelisting

---
+++ components/whitelabeling/configuration_tests.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<BR>REQUEST URI<BR>";
+echo $_SERVER['REQUEST_URI'];
+
+echo "<BR>HTTP_HOST<BR>";
+echo $_SERVER['HTTP_HOST']; //this is great
+//so this means forsomeone like Tushar I can easily pull this stuff together after 
+
+
+echo "<BR>REQUEST URI<BR>";
+echo $_SERVER['REQUEST_URI'];
+
+echo "<BR>REQUEST URI<BR>";
+echo $_SERVER['REQUEST_URI'];
---
+++ insights/calculations/revenue/per_contact/notes.php
@@ -0,0 +1,3 @@
+perhaps not obviously this may not stack up entirely /match up entirely with that of the other revenue reports but it should be very very close
+
+perhaps it is a % of revenue share not just everything that goes into it which matters months
