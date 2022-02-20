a18d3fe3e6dab81bf7aa7be9b4c53c7aa6fe27bd
Chris Riddell <chris.riddell@dreamboat.co>
2022-02-20 21:43:56 +1100

    cleanup prior to whitelabel test branch

---
+++ components/navigation/development/notes_as_we_go.php
@@ -0,0 +1,4 @@
+18.02.22
+ok so if i pull this off then i'll be building the things for clients to properly and fully communicate with their clients'
+so what this means is that the menu will be separate to everything esle
+holy shitballs that's
