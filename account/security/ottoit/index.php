c965de2d7bc23a6070a2501ac6001e50db963ab0
Chris Riddell <chris.riddell@dreamboat.co>
2022-02-24 23:08:09 +1100

    push it all

--- account/security/ottoit/index.php
+++ account/security/ottoit/index.php
@@ -2,3 +2,7 @@
 if(!isset($_SESSION)){session_start();}
 
 echo "pushed all the way through now";
+
+if(!isset($_SESSION['masked_domain'])){$_SESSION['masked_domain'] == 'ottoit';}
+
+require $_SERVER['DOCUMENT_ROOT']."/account/security/index.php";
