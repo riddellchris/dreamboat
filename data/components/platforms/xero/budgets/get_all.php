7e8fac1aed7fa8fef3f03bef54651b223475b303
Chris Riddell <chris.riddell@dreamboat.co>
2022-03-26 18:37:42 +1100

    push to test with Leigh

---
+++ data/components/platforms/xero/budgets/get_all.php
@@ -0,0 +1,20 @@
+<?php
+if(!isset($_SESSION)){session_start();}
+
+require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/tenants/get_tenant_user_ids_from_db.php";
+
+//for each in that array
+for($j = 0; $j < count($tenant_user_id); $j++){
+
+    //set the $_SESSION user_id with the corresponding user_id for this tenant
+    $user_id_for_request    = $tenant_user_id[$j];
+    $tenant_id              = $tenant_xero_id[$j];
+
+    require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/oauth/get_access_token.php";
+
+    $getting_all = 'yes';
+    require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/budgets/get_one.php";
+    unset($getting_all);
+
+
+}
