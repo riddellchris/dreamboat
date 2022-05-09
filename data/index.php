8732945990c933330a0f4c41ca9a18044a641716
Chris Riddell <chris.riddell@dreamboat.co>
2022-05-09 16:08:35 +1000

    Chart changes

--- data/index.php
+++ data/index.php
@@ -1,3 +1,20 @@
 <?php
 if(!isset($_SESSION)){session_start();}
+//echo 'hi';exit();
+
+//echo '<pre>' , var_dump($_SESSION) , '</pre>';
 require $_SERVER['DOCUMENT_ROOT']."/components/functions/primary_folder_redirections.php";
+
+/*
+if(isset($_SESSION['logged_in'])){
+    if($_SESSION['logged_in'] == 'yes'){ 
+        
+    }
+    else{
+        header("/account/security/"); exit();
+    }
+}
+else{
+        header("/slideshows/"); exit();
+}
+*/
