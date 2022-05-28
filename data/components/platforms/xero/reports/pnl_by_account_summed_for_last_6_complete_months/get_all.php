6e3a08e63748ebc3719cae0ada07d51b32d1f01a
Chris Riddell <chris.riddell@dreamboat.co>
2022-05-28 13:09:16 +1000

    main_counter_not_J

--- data/components/platforms/xero/reports/pnl_by_account_summed_for_last_6_complete_months/get_all.php
+++ data/components/platforms/xero/reports/pnl_by_account_summed_for_last_6_complete_months/get_all.php
@@ -43,10 +43,10 @@ function p_and_l_scan($item, $key){
 require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/tenants/get_tenant_user_ids_from_db.php";
 
 //for each in that array
-for($j = 0; $j < count($tenant_user_id); $j++){
+for($main_counter = 0; $main_counter < count($tenant_user_id); $main_counter++){
 
     //set the $_SESSION user_id with the corresponding user_id for this tenant
-    $user_id_for_request    = $tenant_user_id[$j];
+    $user_id_for_request    = $tenant_user_id[$main_counter];
    // $tenant_id              = $tenant_xero_id[$j];
 
 
