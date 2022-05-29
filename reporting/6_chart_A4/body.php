9aba2bd78cee1b337e417f5a6b903d01fb0b0e36
Chris Riddell <chris.riddell@dreamboat.co>
2022-05-29 22:29:02 +1000

    logo and label changes

--- reporting/6_chart_A4/body.php
+++ reporting/6_chart_A4/body.php
@@ -45,20 +45,24 @@ echo "<table >";
                 echo "<table >";
                     echo "<tr>";
                         echo "<td class='title_blocks'>";
-                            if($_SESSION['viewing_client_id'] == '4383'){
+                            $go_to_dummy_logo = 'yes';
 
-                                echo "<img src='/components/whitelabeling/prime_strategies/laila PNG.png' alt='Laila & Me' width='100' height='100' style='margin-bottom: 20px;'>";
-                                echo "<img src='/components/whitelabeling/prime_strategies/WEB_NO BG_Vertical Logo with Tag.png' alt='Laila & Me' width='140' height='140'>";
+                            if($_SESSION['viewing_client_id'] == '4231'){   echo "<img src='/components/whitelabeling/national_pc/NationalPCLogo.png' alt='National PC Logo' width='100' height='100' style='margin-bottom: 20px;'>";
+                                                                            $go_to_dummy_logo = 'no';
                             }
-                            else{
-                                echo "<div style='margin-left: 25px;'>Your Logo</div>";
 
+                            if($_SESSION['viewing_client_id'] == '4383'){   echo "<img src='/components/whitelabeling/prime_strategies/laila PNG.png' alt='Laila & Me' width='100' height='100' style='margin-bottom: 20px;'>";
+                                                                            echo "<img src='/components/whitelabeling/prime_strategies/WEB_NO BG_Vertical Logo with Tag.png' alt='Laila & Me' width='140' height='140'>";
+                                                                            $go_to_dummy_logo = 'no';
                             }
+                            if($go_to_dummy_logo == 'yes'){                 echo "<div style='margin-left: 25px;'>Your Logo</div>";}
 
                         echo "</td>";
                         echo "<td class='title_blocks' style='text-align:center;'>";
                             echo "<span style='font-size: 30px; text-decoration: underline; padding-bottom: 5px; font-weight: 800;'>";
-                                if($_SESSION['viewing_client_id'] == '4383'){echo "Laila and Me / Rawvolution";}
+                                if($_SESSION['viewing_client_id'] == '1'){      echo "Dreamboat";}
+                                if($_SESSION['viewing_client_id'] == '4231'){   echo "National PC";}
+                                if($_SESSION['viewing_client_id'] == '4383'){   echo "Laila and Me / Rawvolution";}
                                 else{echo "Your Company Name";}
                             echo "</span>";
                             
