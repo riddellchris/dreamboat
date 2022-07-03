1df8d4bb96b13a294ca26b2e8f96f9eff86a0715
Chris Riddell <chris.riddell@dreamboat.co>
2022-07-03 21:52:25 +1000

    push fix

--- data/components/platforms/xero/oauth/access_token/get.php
+++ data/components/platforms/xero/oauth/access_token/get.php
@@ -24,43 +24,50 @@ $sql = "SELECT * FROM api_xero_return_keys
         DESC LIMIT 1";
 //echo $sql."<br>";
 //exit();
+
+
+
 $result = mysqli_query($conn, $sql);     
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-/*
-echo "<br>Return KEY entered into db (according to the db)<BR>";
-echo $row['timestamp_entered'];
-echo " === UNIX TIME: ";
-echo strtotime($row['timestamp_entered']);
-echo "<br>Unix time time according to the client: &nbsp;&nbsp;";
-echo date('U');
-echo "<br>Client string time: &nbsp;&nbsp;";echo date('Y-m-d H:i:s');
-*/
+if(mysqli_num_rows($result) != 0){
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-//if port :888 th
-$time_difference = date('U') - strtotime($row['timestamp_entered']);
+    /*
+    echo "<br>Return KEY entered into db (according to the db)<BR>";
+    echo $row['timestamp_entered'];
+    echo " === UNIX TIME: ";
+    echo strtotime($row['timestamp_entered']);
+    echo "<br>Unix time time according to the client: &nbsp;&nbsp;";
+    echo date('U');
+    echo "<br>Client string time: &nbsp;&nbsp;";echo date('Y-m-d H:i:s');
+    */
 
-/*
-if($_SERVER['SERVER_PORT'] != '8888'){
-    $time_difference = $time_difference + 60*60*10;
-}
-*/
-////echo "<br>";
-$use_current_token = 'yes';
-//echo $time_difference." seconds";
-if(abs($time_difference) > 29 * 60){$use_current_token = 'no';}
-else{$use_current_token = 'yes';}
+    //if port :888 th
+    $time_difference = date('U') - strtotime($row['timestamp_entered']);
 
-if($use_current_token == 'yes'){$get_new_token = 'no';}
-else{$get_new_token = 'yes';}
+    /*
+    if($_SERVER['SERVER_PORT'] != '8888'){
+        $time_difference = $time_difference + 60*60*10;
+    }
+    */
+    ////echo "<br>";
+    $use_current_token = 'yes';
+    //echo $time_difference." seconds";
+    if(abs($time_difference) > 29 * 60){$use_current_token = 'no';}
+    else{$use_current_token = 'yes';}
 
-//echo "<BR>GET NEW TOKEN (Yes/No) = ".$get_new_token;
-if($use_current_token == 'yes'){
+    if($use_current_token == 'yes'){$get_new_token = 'no';}
+    else{$get_new_token = 'yes';}
 
-    $access_token = $row['access_token'];
+    //echo "<BR>GET NEW TOKEN (Yes/No) = ".$get_new_token;
+    if($use_current_token == 'yes'){
+
+        $access_token = $row['access_token'];
 
 
-}
-else{
-    require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/oauth/access_token/refresh.php";
+    }
+    else{
+        require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/oauth/access_token/refresh.php";
+    }
+
 }
