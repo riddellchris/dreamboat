8af366ae1646c9b9b617ba28075ec110462e734c
Chris Riddell <chris.riddell@dreamboat.co>
2022-07-22 11:31:13 +1000

    shopify comparison

--- financials/revenue/shopify_comparison/script.php
+++ financials/revenue/shopify_comparison/script.php
@@ -27,106 +27,15 @@ else{
 
           <?php
                 echo " ['Month', 'Revenue', { role: 'annotation' }, 'Shopify Revenue', { role: 'annotation' }],";
-                if(
-                     //Chris
-                    $_SESSION['viewing_client_id'] != 4383 AND
-                    $_SESSION['viewing_client_id'] != 4398 
-                    ){  //Tricia Ong / mel
-                      echo "
-                      ['Dec 21',   450,   '450',      375,   '375'],          
-                      ['Jan 22',   450,    '450',     375,   '375'],
-                      ['Feb 22',   288,    '450',     375,   '375'],
-                      ['Mar 22',   397,    '450',     375,   '375'],
-                      ['Apr 22',   215,     '450',    375,   '375'],
-                      ['May 22',   366,     '450',    375,   '375']
-                      ";
 
-                      $sub_title = "Dummy data only";
+                  $setup_completed = 'no';
+                  if($_SESSION['viewing_client_id'] == 4383){ require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/shopify_comparison/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+                  if($_SESSION['viewing_client_id'] == 4398){ require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/shopify_comparison/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+                  if($setup_completed == 'no'){               require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/shopify_comparison/dummy_data.php";}
+                  unset($setup_completed);
 
 
-                    }
-                else{
-
-                  if( $_SESSION['viewing_client_id'] == 4383){
-
-                    $sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
-                            WHERE         user_id = '".$_SESSION['viewing_client_id']."'
-                            AND           latest_version_for_this_user = 'yes'
-                            AND          ( account_name = 'Total Income' OR  account_name = 'LAM _Shopify' ) 
-                            AND           date_index > -7
-                            ORDER BY date_index ASC          
-                            ";
-                    //echo $sql; exit();
-                    $result = mysqli_query($conn, $sql);
-
-                    unset($extracted);
-
-
-
-
-
-
-                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                      $date_index = $row['date_index'];
-                      if($row['account_name'] == 'Total Income'){ $extracted['Total Income'][$date_index] = $row['value'];}
-                      if($row['account_name'] == 'LAM _Shopify'){ $extracted['LAM _Shopify'][$date_index] = $row['value'];} 
-                                                                  $extracted['period_for_chart_display'][$date_index] = $row['period_for_chart_display'];                                    
-                    }
-
-                    for($extraction_counter = -6; $extraction_counter < 0; $extraction_counter ++){
-                      echo " ['".$extracted['period_for_chart_display'][$extraction_counter]."',       
-                                ".$extracted['Total Income'][$extraction_counter].",      '".$extracted['Total Income'][$extraction_counter]."',       
-                                ".$extracted['LAM _Shopify'][$extraction_counter].",      '".$extracted['LAM _Shopify'][$extraction_counter]."'],      ";
-
-                      $sub_title = $row['accurate_as_at_string'];
-
-                      //RAW_Contract Manufacturing not included as not a sales item
-
-                    }   
-                  }
-                  if( $_SESSION['viewing_client_id'] == 4398){
-
-                    $sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
-                            WHERE         user_id = '".$_SESSION['viewing_client_id']."'
-                            AND           latest_version_for_this_user = 'yes'
-                            AND          ( account_name = 'Total Income' OR  account_name = 'LAM _Shopify' ) 
-                            AND           date_index > -7
-                            ORDER BY date_index ASC          
-                            ";
-                    //echo $sql; exit();
-                    $result = mysqli_query($conn, $sql);
-
-                    unset($extracted);
-
-
-
-
-
-
-                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                      $date_index = $row['date_index'];
-                      if($row['account_name'] == 'Total Income'){ $extracted['Total Income'][$date_index] = $row['value'];}
-                      if($row['account_name'] == 'LAM _Shopify'){ $extracted['LAM _Shopify'][$date_index] = $row['value'];} 
-                                                                  $extracted['period_for_chart_display'][$date_index] = $row['period_for_chart_display'];                                    
-                    }
-
-                    for($extraction_counter = -6; $extraction_counter < 0; $extraction_counter ++){
-                      echo " ['".$extracted['period_for_chart_display'][$extraction_counter]."',       
-                                ".$extracted['Total Income'][$extraction_counter].",      '".$extracted['Total Income'][$extraction_counter]."',       
-                                ".$extracted['LAM _Shopify'][$extraction_counter].",      '".$extracted['LAM _Shopify'][$extraction_counter]."'],      ";
-
-                      $sub_title = $row['accurate_as_at_string'];
-
-                      //RAW_Contract Manufacturing not included as not a sales item
-
-                    }   
-                  }
-
-
-
-
-
-                }
+             
             ?>
         ]);
 
