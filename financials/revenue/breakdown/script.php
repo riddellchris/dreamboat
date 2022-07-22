cefd920f0327d1992c1ea85e8e390d16fa975e91
Chris Riddell <chris.riddell@dreamboat.co>
2022-07-22 10:02:49 +1000

    4398 updates

--- financials/revenue/breakdown/script.php
+++ financials/revenue/breakdown/script.php
@@ -31,81 +31,11 @@ else{
 
           <?php
 
-                if($_SESSION['viewing_client_id'] != 4383){  //Tricia Ong / mel
-                      echo "['Month',     'Website', { role: 'annotation' }, 'Wholesale', { role: 'annotation' }, 'Distribution', { role: 'annotation' }, 'Other', { role: 'annotation' }],";
-                      echo "
-                      ['Dec 21',  450,  '450',  614.6,  '614',  614.6, '614', 614.6, '614'],          
-                      ['Jan 22',  450,  '450',  614.6,  '614',  614.6, '614', 614.6, '614'],
-                      ['Feb 22',  288,  '288',  682,    '682',  614.6, '614', 614.6, '614'],
-                      ['Mar 22',  397,  '397',  623,    '623',  614.6, '614', 614.6, '614'],
-                      ['Apr 22',  215,  '215',  609.4,  '609',  614.6, '614', 614.6, '614'],
-                      ['May 22',  366,  '366',  569.6,  '569',  614.6, '614', 614.6, '614']
-                      ";
-
-                      $sub_title = "Dummy data only";
-
-
-                    }
-                else{
-
-                  if( $_SESSION['viewing_client_id'] == 4383){
-
-                    $months_to_display = 3;
-                    //therefore
-                    $max_extract_to_for_sql = $months_to_display * -1 - 1;
-                    $extraction_counter_start = $months_to_display * -1; 
-
-                    $sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
-                            WHERE         user_id = '".$_SESSION['viewing_client_id']."'
-                            AND           latest_version_for_this_user = 'yes'
-                            AND           date_index > ".$max_extract_to_for_sql."
-                            ORDER BY date_index ASC          
-                            ";
-                    //echo $sql; exit();
-                    $result = mysqli_query($conn, $sql);
-
-                    unset($extracted);
-
-                    $extracted['website']       = array();
-                    $extracted['other']         = array();
-                    $extracted['distribution']  = array();
-                    $extracted['wholesale']     = array();
-
-                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                      $date_index = $row['date_index'];
-                      if($row['account_name'] == 'LAM _Shopify'){               if(!isset($extracted['website'][$date_index])){      $extracted['website'][$date_index]       = 0;} $extracted['website'][$date_index]       += $row['value'];}
-                      if($row['account_name'] == 'LAM_Distributors'){           if(!isset($extracted['distribution'][$date_index])){ $extracted['distribution'][$date_index]  = 0;} $extracted['distribution'][$date_index]  += $row['value'];}
-                      if($row['account_name'] == 'RAW_Distributors'){           if(!isset($extracted['distribution'][$date_index])){ $extracted['distribution'][$date_index]  = 0;} $extracted['distribution'][$date_index]  += $row['value'];}                
-                      if($row['account_name'] == 'LAM_Wholesale'){              if(!isset($extracted['wholesale'][$date_index])){    $extracted['wholesale'][$date_index]     = 0;} $extracted['wholesale'][$date_index]     += $row['value'];}
-                      if($row['account_name'] == 'Sale from wholesale'){        if(!isset($extracted['wholesale'][$date_index])){    $extracted['wholesale'][$date_index]     = 0;} $extracted['wholesale'][$date_index]     += $row['value'];}                   
-                      if($row['account_name'] == 'RAW_Other'){                  if(!isset($extracted['other'][$date_index])){        $extracted['other'][$date_index]         = 0;} $extracted['other'][$date_index]         += $row['value'];}
-                      if($row['account_name'] == 'Sales from Market Stalls'){   if(!isset($extracted['other'][$date_index])){        $extracted['other'][$date_index]         = 0;} $extracted['other'][$date_index]         += $row['value'];}
-                      if($row['account_name'] == 'RAW_Contract Manufacturing'){ if(!isset($extracted['manufacturing'][$date_index])){$extracted['manufacturing'][$date_index] = 0;} $extracted['manufacturing'][$date_index] += $row['value'];}                      
-                      
-                      $extracted['period_for_chart_display'][$date_index] = $row['period_for_chart_display'];
-
-                    }
-                    echo "['Month',     'Website', { role: 'annotation' }, 'Wholesale', { role: 'annotation' }, 'Distribution', { role: 'annotation' }, 'Manufacturing', { role: 'annotation' }, 'Other', { role: 'annotation' }],";
-                    for($extraction_counter = $extraction_counter_start; $extraction_counter < 0; $extraction_counter ++){
-                      echo " ['".$extracted['period_for_chart_display'][$extraction_counter]."',       
-                                ".$extracted['website'][$extraction_counter].",                     '".$extracted['website'][$extraction_counter]."',    
-                                ".$extracted['wholesale'][$extraction_counter].",                   '".$extracted['wholesale'][$extraction_counter]."', 
-                                ".$extracted['distribution'][$extraction_counter].",                '".$extracted['distribution'][$extraction_counter]."',  
-                                ".$extracted['manufacturing'][$extraction_counter].",               '".$extracted['manufacturing'][$extraction_counter]."',
-                                ".$extracted['other'][$extraction_counter].",                       '".$extracted['other'][$extraction_counter]."'    
-
-                              
-                              
-                              
-                              ],      ";
-
-                      $sub_title = $row['accurate_as_at_string'];
-
-                      //RAW_Contract Manufacturing not included as not a sales item
-
-                    }   
-                  }
-                }
+              $setup_completed = 'no';
+              if($_SESSION['viewing_client_id'] == 4383){ require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/breakdown/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+              if($_SESSION['viewing_client_id'] == 4398){ require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/breakdown/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+              if($setup_completed == 'no'){               require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/breakdown/dummy_data.php";}
+              unset($setup_completed);
             ?>
 
 
---
+++ financials/revenue/breakdown/user_specific_calculations/4383.php
@@ -0,0 +1,57 @@
+<?php
+if(!isset($_SESSION)){session_start();}
+
+$months_to_display = 3;
+//therefore
+$max_extract_to_for_sql = $months_to_display * -1 - 1;
+$extraction_counter_start = $months_to_display * -1; 
+
+$sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
+        WHERE         user_id = '".$_SESSION['viewing_client_id']."'
+        AND           latest_version_for_this_user = 'yes'
+        AND           date_index > ".$max_extract_to_for_sql."
+        ORDER BY date_index ASC          
+        ";
+//echo $sql; exit();
+$result = mysqli_query($conn, $sql);
+
+unset($extracted);
+
+$extracted['website']       = array();
+$extracted['other']         = array();
+$extracted['distribution']  = array();
+$extracted['wholesale']     = array();
+
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+    $date_index = $row['date_index'];
+    if($row['account_name'] == 'LAM _Shopify'){               if(!isset($extracted['website'][$date_index])){      $extracted['website'][$date_index]       = 0;} $extracted['website'][$date_index]       += $row['value'];}
+    if($row['account_name'] == 'LAM_Distributors'){           if(!isset($extracted['distribution'][$date_index])){ $extracted['distribution'][$date_index]  = 0;} $extracted['distribution'][$date_index]  += $row['value'];}
+    if($row['account_name'] == 'RAW_Distributors'){           if(!isset($extracted['distribution'][$date_index])){ $extracted['distribution'][$date_index]  = 0;} $extracted['distribution'][$date_index]  += $row['value'];}                
+    if($row['account_name'] == 'LAM_Wholesale'){              if(!isset($extracted['wholesale'][$date_index])){    $extracted['wholesale'][$date_index]     = 0;} $extracted['wholesale'][$date_index]     += $row['value'];}
+    if($row['account_name'] == 'Sale from wholesale'){        if(!isset($extracted['wholesale'][$date_index])){    $extracted['wholesale'][$date_index]     = 0;} $extracted['wholesale'][$date_index]     += $row['value'];}                   
+    if($row['account_name'] == 'RAW_Other'){                  if(!isset($extracted['other'][$date_index])){        $extracted['other'][$date_index]         = 0;} $extracted['other'][$date_index]         += $row['value'];}
+    if($row['account_name'] == 'Sales from Market Stalls'){   if(!isset($extracted['other'][$date_index])){        $extracted['other'][$date_index]         = 0;} $extracted['other'][$date_index]         += $row['value'];}
+    if($row['account_name'] == 'RAW_Contract Manufacturing'){ if(!isset($extracted['manufacturing'][$date_index])){$extracted['manufacturing'][$date_index] = 0;} $extracted['manufacturing'][$date_index] += $row['value'];}                      
+    
+    $extracted['period_for_chart_display'][$date_index] = $row['period_for_chart_display'];
+
+}
+echo "['Month',     'Website', { role: 'annotation' }, 'Wholesale', { role: 'annotation' }, 'Distribution', { role: 'annotation' }, 'Manufacturing', { role: 'annotation' }, 'Other', { role: 'annotation' }],";
+for($extraction_counter = $extraction_counter_start; $extraction_counter < 0; $extraction_counter ++){
+    echo " ['".$extracted['period_for_chart_display'][$extraction_counter]."',       
+            ".$extracted['website'][$extraction_counter].",                     '".$extracted['website'][$extraction_counter]."',    
+            ".$extracted['wholesale'][$extraction_counter].",                   '".$extracted['wholesale'][$extraction_counter]."', 
+            ".$extracted['distribution'][$extraction_counter].",                '".$extracted['distribution'][$extraction_counter]."',  
+            ".$extracted['manufacturing'][$extraction_counter].",               '".$extracted['manufacturing'][$extraction_counter]."',
+            ".$extracted['other'][$extraction_counter].",                       '".$extracted['other'][$extraction_counter]."'    
+
+            
+            
+            
+            ],      ";
+
+    $sub_title = $row['accurate_as_at_string'];
+
+    //RAW_Contract Manufacturing not included as not a sales item
+
+} 
