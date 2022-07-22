c95d484f9ba07b9b5e43b842a54071015f47f368
Chris Riddell <chris.riddell@dreamboat.co>
2022-07-22 11:05:56 +1000

    adspend for nicole

--- financials/expenses/adspend_vs_return/script.php
+++ financials/expenses/adspend_vs_return/script.php
@@ -30,74 +30,11 @@ else{
 
           <?php
 
-                if($_SESSION['viewing_client_id'] != 4383){  //Tricia Ong / mel
-                  echo "['Month',   'Return', { role: 'annotation' }, 'Websales', { role: 'annotation' }, 'Adspend', { role: 'annotation' }],
-                        ['Dec 21',  4,  '4',  614.6,  '614',  614.6, '614'],          
-                        ['Jan 22',  5,  '5',  614.6,  '614',  614.6, '614'],
-                        ['Feb 22',  6,  '6',  682,    '682',  614.6, '614'],
-                        ['Mar 22',  5,  '5',  623,    '623',  614.6, '614'],
-                        ['Apr 22',  5,  '5',  609.4,  '609',  614.6, '614'],
-                        ['May 22',  5,  '5',  569.6,  '569',  614.6, '614']
-                      ";
-
-                      $sub_title = "Dummy data only";
-
-                    }
-                else{
-
-                  if( $_SESSION['viewing_client_id'] == 4383){
-
-                    $sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
-                            WHERE         user_id = '".$_SESSION['viewing_client_id']."'
-                            AND           latest_version_for_this_user = 'yes'
-                            AND           date_index > -7
-                            ORDER BY date_index ASC          
-                            ";
-                    //echo $sql; exit();
-                    $result = mysqli_query($conn, $sql);
-
-                    unset($extracted);
-
-                    $extracted['website']       = array();
-                    $extracted['adspend']       = array();
-
-                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                      $date_index = $row['date_index'];
-                      if($row['account_name'] == 'LAM _Shopify'){                       if(!isset($extracted['website'][$date_index])){ $extracted['website'][$date_index]  = 0;} $extracted['website'][$date_index]  += $row['value'];}
-                      if($row['account_name'] == 'Advertising and Marketing - LAM'){    if(!isset($extracted['adspend'][$date_index])){ $extracted['adspend'][$date_index]  = 0;} $extracted['adspend'][$date_index]  += $row['value'];}                      
-                      $extracted['period_for_chart_display'][$date_index] = $row['period_for_chart_display'];
-                    }
-
-
-
-
-
-                    echo "['Month',   'Return', { role: 'annotation' }, 'Websales', { role: 'annotation' }, 'Adspend', { role: 'annotation' }],";
-                    for($extraction_counter = -6; $extraction_counter < 0; $extraction_counter ++){
-
-                        $correct_elements = 0;
-                        if(isset($extracted['adspend'][$extraction_counter] )){$adspend = $extracted['adspend'][$extraction_counter]; $correct_elements ++;}
-                        if(isset($extracted['website'][$extraction_counter] )){$website = $extracted['website'][$extraction_counter]; $correct_elements ++;}
-                        if($correct_elements == 2){
-                          $computed['return_on_adspend'][$extraction_counter] = round($extracted['website'][$extraction_counter] / $extracted['adspend'][$extraction_counter], 1);
-
-                        }
-
-
-                      echo " ['".$extracted['period_for_chart_display'][$extraction_counter]."',      
-                                ".$computed['return_on_adspend'][$extraction_counter].",  '".$computed['return_on_adspend'][$extraction_counter]."',                       
-                                ".$extracted['website'][$extraction_counter].",           '".$extracted['website'][$extraction_counter]."',    
-                                ".$extracted['adspend'][$extraction_counter].",           '".$extracted['adspend'][$extraction_counter]."' ]      ";
-
-                      if($extraction_counter <> 0){echo ",";}
-
-                      $sub_title = $row['accurate_as_at_string'];
-
-                      //RAW_Contract Manufacturing not included as not a sales item
-
-                    }   
-                  }
-                }
+            $setup_completed = 'no';
+            if($_SESSION['viewing_client_id'] == 4383){ require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/adspend_vs_return/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+            if($_SESSION['viewing_client_id'] == 4398){ require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/adspend_vs_return/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+            if($setup_completed == 'no'){               require $_SERVER['DOCUMENT_ROOT']."/financials/revenue/adspend_vs_return/dummy_data.php";}
+            unset($setup_completed);
             ?>
 
 
