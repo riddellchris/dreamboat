de7c49dd809750f9acbab1e7baa3696f20d70a70
Chris Riddell <chris.riddell@dreamboat.co>
2022-07-22 14:14:40 +1000

    debtor days attempt 1

---
+++ financials/cashflow/debtor_days/dummy_data.php
@@ -0,0 +1,20 @@
+<?php
+if(!isset($_SESSION)){session_start();}
+
+  echo "
+  ['Month',     'Debtor Days', { role: 'annotation' },'Target'],
+  ['Dec 21',   95, '95',  45],          
+  ['Jan 22',   90, '90',  45],
+  ['Feb 22',   92, '92',  45],
+  ['Mar 22',   85, '85',  45],
+  ['Apr 22',   80, '80',  45],
+  ['May 22',   78, '78',  45],
+  ['Jun 22',   76, '76',  45],
+  ['Jul 22',   74, '74',  45],
+  ['Aug 22',   72, '72',  45],
+  ['Sep 22',   70, '70',  45],
+  ['Oct 22',   68, '68',  45],
+  ['Nov 22',   66, '66',  45],
+  ";
+
+  $sub_title = "Dummy data only";
--- financials/cashflow/debtor_days/script.php
+++ financials/cashflow/debtor_days/script.php
@@ -202,23 +202,15 @@ if( $_SESSION['viewing_client_id'] == 4231 OR
 }
 else{
   */
-  echo "
-  ['Month',     'Debtor Days', { role: 'annotation' },'Target'],
-  ['Dec 21',   95, '95',  45],          
-  ['Jan 22',   90, '90',  45],
-  ['Feb 22',   92, '92',  45],
-  ['Mar 22',   85, '85',  45],
-  ['Apr 22',   80, '80',  45],
-  ['May 22',   78, '78',  45],
-  ['Jun 22',   76, '76',  45],
-  ['Jul 22',   74, '74',  45],
-  ['Aug 22',   72, '72',  45],
-  ['Sep 22',   70, '70',  45],
-  ['Oct 22',   68, '68',  45],
-  ['Nov 22',   66, '66',  45],
-  ";
 
-  $sub_title = "Dummy data only";
+
+  $setup_completed = 'no';
+  if($_SESSION['viewing_client_id'] == 1){ require $_SERVER['DOCUMENT_ROOT']."/financials/cashflow/debtor_days/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+  //if($_SESSION['viewing_client_id'] == 4398){ require $_SERVER['DOCUMENT_ROOT']."/financials/cashflow/debtor_days/user_specific_calculations/".$_SESSION['viewing_client_id'].".php";  $setup_completed = 'yes';}
+  if($setup_completed == 'no'){               require $_SERVER['DOCUMENT_ROOT']."/financials/cashflow/debtor_days/dummy_data.php";}
+  unset($setup_completed);
+
+ 
 
 
 
