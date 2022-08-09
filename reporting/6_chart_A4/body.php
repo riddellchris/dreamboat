32842edef8a5dcb9931dc745dd3822831f89259e
riddellchris <chris.riddell@dreamboat.co>
2022-08-09 09:33:54 +0800

    steve rouget layout adjustments

--- reporting/6_chart_A4/body.php
+++ reporting/6_chart_A4/body.php
@@ -49,12 +49,20 @@ echo "<table>";
         $_SESSION['viewing_client_id'] == '4399' 
         ){
         $default_setup = 'no';
-        $_SESSION['reporting']['6_chart_A4']['chart_position'][1] = 'financials_profit_and_loss_gross_profit_div';
+
+
+        $_SESSION['reporting']['6_chart_A4']['chart_position'][1] = 'financials_profit_and_loss_revenue_div';   
+        $_SESSION['reporting']['6_chart_A4']['chart_position'][3] = 'financials_profit_and_loss_gross_profit_div';
+        $_SESSION['reporting']['6_chart_A4']['chart_position'][5] = 'financials_expenses_operating_div';
+        $_SESSION['reporting']['6_chart_A4']['chart_position'][7] = 'financials_profit_and_loss_net_profit_div';
+
+
+
         $_SESSION['reporting']['6_chart_A4']['chart_position'][2] = 'financials_cashflow_debtor_days_div';
-        $_SESSION['reporting']['6_chart_A4']['chart_position'][3] = 'financials_expenses_operating_div';
         $_SESSION['reporting']['6_chart_A4']['chart_position'][4] = 'financials_profit_and_loss_gross_profit_div';
-        $_SESSION['reporting']['6_chart_A4']['chart_position'][5] = 'financials_profit_and_loss_net_profit_div';
         $_SESSION['reporting']['6_chart_A4']['chart_position'][6] = 'financials_expenses_wages_div';
+        $_SESSION['reporting']['6_chart_A4']['chart_position'][6] = 'financials_expenses_wages_as_percent_of_revenue_div';
+
     }
 
     //Leigh Kefford
