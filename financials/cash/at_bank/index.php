c800436814fa4cc4a4720f6622f6474cdd369417
Chris Riddell <chris.riddell@dreamboat.co>
2022-08-30 10:15:48 +1000

    cash at bank changes

--- financials/cash/at_bank/index.php
+++ financials/cash/at_bank/index.php
@@ -3,5 +3,14 @@ if(!isset($_SESSION)){session_start();}
 require $_SERVER['DOCUMENT_ROOT']."/account/security/logged_in_check.php";
 require $_SERVER['DOCUMENT_ROOT']."/components/layouts/standard_page/start.php";
 
+
+
+
+echo "<div id='".$location_string."_div' style='height:75vh;'></div>";
+
+
+require $_SERVER['DOCUMENT_ROOT']."/components/tracking/input_table/display.php";
+
+
 require $_SERVER['DOCUMENT_ROOT']."/components/layouts/standard_page/end.php";
 ?>
