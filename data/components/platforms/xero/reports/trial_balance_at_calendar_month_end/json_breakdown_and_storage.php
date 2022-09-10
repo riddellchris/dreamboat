0e800850e019df83e20cb5955c99ff8bdf2c2929
Chris Riddell <chris.riddell@dreamboat.co>
2022-09-10 14:04:51 +1000

    trial balance latest version update

--- data/components/platforms/xero/reports/trial_balance_at_calendar_month_end/json_breakdown_and_storage.php
+++ data/components/platforms/xero/reports/trial_balance_at_calendar_month_end/json_breakdown_and_storage.php
@@ -22,6 +22,15 @@ else{$proper_reponse = 'no';}
 require $_SERVER['DOCUMENT_ROOT']."/data/components/platforms/xero/components/set_error_handler.php";
 
 if($proper_reponse == 'yes'){
+
+
+    $sql = "UPDATE api_xero_reports_trial_balance_at_calendar_month_end
+            SET latest_version_for_this_user = 'no'
+            WHERE user_id = '".$user_id_for_request."'
+            ";
+    mysqli_query($conn, $sql);  
+
+
    //Revenues
    for($j = 0; $j < count($response); $j ++ ){
   
