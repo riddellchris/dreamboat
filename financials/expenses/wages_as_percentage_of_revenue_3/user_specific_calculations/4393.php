af1d5015c7974833de7e78c1d6bbcda37c0eb52c
Chris Riddell <chris.riddell@dreamboat.co>
2022-09-20 08:46:13 +1000

    4393 major change

--- financials/expenses/wages_as_percentage_of_revenue_3/user_specific_calculations/4393.php
+++ financials/expenses/wages_as_percentage_of_revenue_3/user_specific_calculations/4393.php
@@ -1,33 +1,31 @@
 <?php
 if(!isset($_SESSION)){session_start();}
 
-$sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
-WHERE         user_id = '".$_SESSION['viewing_client_id']."'
-AND           latest_version_for_this_user = 'yes'
-AND           date_index > -7
-ORDER BY date_index ASC          
-";
-//echo $sql; exit();
-$result = mysqli_query($conn, $sql);
+require $_SERVER['DOCUMENT_ROOT']."/financials/expenses/wages_as_percentage_of_revenue_3/user_specific_calculations/parts/sql_query.php";
 
 unset($extracted);
 
-$extracted['wages']                     = array();
-$extracted['percent_of_revenue']        = array();
+$extracted['denominator']               = array(); //originally total_income
+$extracted['numerator']                 = array(); //originally wages
+
+$denominator_names = array();
+$numerator_names   = array();
+
+$denominator_names[0]   = 'Total Income';
+$numerator_names[0]     = 'Subcontractor - no GST';
+$numerator_names[1]     = 'Subcontractor - GST';
+
 
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 $date_index = $row['date_index'];
-if($row['account_name'] == 'Total Income'){          if(!isset($extracted['total_income'][$date_index])){ $extracted['total_income'][$date_index]  = 0;} $extracted['total_income'][$date_index]  += $row['value'];}
-//if($row['account_name'] == 'Wages and Salaries'){    if(!isset($extracted['wages'][$date_index])){ $extracted['wages'][$date_index]  = 0;} $extracted['wages'][$date_index]  += $row['value'];}    
-if($row['account_name'] == 'Subcontractor - no GST'){           if(!isset($extracted['wages'][$date_index])){ $extracted['wages'][$date_index]  = 0;} $extracted['wages'][$date_index]  += $row['value'];}    
-if($row['account_name'] == 'Subcontractor - GST'){           if(!isset($extracted['wages'][$date_index])){ $extracted['wages'][$date_index]  = 0;} $extracted['wages'][$date_index]  += $row['value'];}  
+if($row['account_name'] == $denominator_name){     if(!isset($extracted['denominator'][$date_index])){  $extracted['denominator'][$date_index]  = 0;} $extracted['denominator'][$date_index]  += $row['value'];} 
+
+//obviously this can be turned into some sort of array simplification.
+if($row['account_name'] == $numerator_names[0]){   if(!isset($extracted['numerator'] [$date_index])){        $extracted['numerator'] [$date_index]  = 0;} $extracted['numerator'] [$date_index]  += $row['value'];}    
+if($row['account_name'] == $numerator_names[1]){   if(!isset($extracted['numerator'] [$date_index])){        $extracted['numerator'] [$date_index]  = 0;} $extracted['numerator'] [$date_index]  += $row['value'];}  
                   
 $extracted['period_for_chart_display'][$date_index] = $row['period_for_chart_display'];
 }
-
-
-
-
 
 echo "['Month',   'Percentage', { role: 'annotation' } ";
 /*
@@ -37,20 +35,19 @@ echo "],";
 for($extraction_counter = -6; $extraction_counter < 0; $extraction_counter ++){
 
 $correct_elements = 0;
-if(isset($extracted['wages'][$extraction_counter] )){$wages = $extracted['wages'][$extraction_counter]; $correct_elements ++;}
-if(isset($extracted['total_income'][$extraction_counter] )){$total_income = $extracted['total_income'][$extraction_counter]; $correct_elements ++;}
+if(isset($extracted['numerator'][$extraction_counter]   )){$wages           = $extracted['numerator'][$extraction_counter];     $correct_elements ++;}
+if(isset($extracted['denominator'][$extraction_counter] )){$total_income    = $extracted['denominator'][$extraction_counter];   $correct_elements ++;}
 if($correct_elements == 2){
-$computed['wages_percentage'][$extraction_counter] = round($extracted['wages'][$extraction_counter] / $extracted['total_income'][$extraction_counter] * 100, 1);
-
+$computed['wages_percentage'][$extraction_counter] = round($extracted['numerator'] [$extraction_counter] / $extracted['denominator'][$extraction_counter] * 100, 1);
 }
 
 
 echo " ['".$extracted['period_for_chart_display'][$extraction_counter]."',      
-    ".$computed['wages_percentage'][$extraction_counter].",  '".$computed['wages_percentage'][$extraction_counter]."'";
+         ".$computed['wages_percentage'][$extraction_counter].",  '".$computed['wages_percentage'][$extraction_counter]."'";
     /*
     ,                       
-    ".$extracted['total_income'][$extraction_counter].",           '".$extracted['total_income'][$extraction_counter]."',    
-    ".$extracted['wages'][$extraction_counter].",           '".$extracted['wages'][$extraction_counter]."' 
+    ".$extracted['denominator'][$extraction_counter].",           '".$extracted['denominator'][$extraction_counter]."',    
+    ".$extracted['numerator'] [$extraction_counter].",           '".$extracted['numerator'] [$extraction_counter]."' 
     */
     echo "]      ";
     
--- financials/expenses/wages_as_percentage_of_revenue_3/user_specific_calculations/4402.php
+++ financials/expenses/wages_as_percentage_of_revenue_3/user_specific_calculations/4402.php
@@ -1,14 +1,7 @@
 <?php
 if(!isset($_SESSION)){session_start();}
 
-$sql = "SELECT * FROM api_xero_reports_pnl_by_calendar_month
-WHERE         user_id = '".$_SESSION['viewing_client_id']."'
-AND           latest_version_for_this_user = 'yes'
-AND           date_index > -7
-ORDER BY date_index ASC          
-";
-//echo $sql; exit();
-$result = mysqli_query($conn, $sql);
+require $_SERVER['DOCUMENT_ROOT']."/financials/expenses/wages_as_percentage_of_revenue_3/user_specific_calculations/parts/sql_query.php";
 
 unset($extracted);
 
