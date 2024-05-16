96a690bf59ae8c164b0d336917d6b2841954d989
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-05-16 18:07:44 +1000

    Update index.php

--- industries/aged_care/display_website/index.php
+++ industries/aged_care/display_website/index.php
@@ -1,7 +1,18 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT']."/components/layouts/standard_page/start.php";
 
-
-
+<img 
+    src="industries/aged_care/display_website/Mars.jpg" 
+    alt="Mars image" 
+    style='
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      ' 
+    height='200px'>
 
 require $_SERVER['DOCUMENT_ROOT']."/components/layouts/standard_page/end.php";
