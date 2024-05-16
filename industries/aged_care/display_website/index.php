f65b52d173d0d1c39bb71bd44c81df47761b7e5d
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-05-16 18:17:51 +1000

    Update index.php

--- industries/aged_care/display_website/index.php
+++ industries/aged_care/display_website/index.php
@@ -9,10 +9,13 @@ require $_SERVER['DOCUMENT_ROOT']."/components/layouts/standard_page/start.php";
       display: block;
       margin-left: auto;
       margin-right: auto;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
+     // position: absolute;
+      top: -50%;
+      left: -50%;
+      width: 200vh
+      height: 200%;
+      object-fit: cover;
+     // transform: translate(-50%, -50%);
       ' 
 
     source='https://www.flickr.com/photos/kevinmgill/32907950425'
