9145de7aff459525fcba1424d18bfdf324be2404
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-15 23:40:27 +1100

    back one step

--- components/layouts/standard_page/initial_folder_settings.php
+++ components/layouts/standard_page/initial_folder_settings.php
@@ -22,7 +22,7 @@ if(substr_count($_SERVER['REQUEST_URI'], '?') > 1){
 
 
 url_folder_get_string_creation();
-
+if($_SERVER['HTTP_HOST']!= "dreamboat.com.au"){echo "url to redirect to:".$url_to_redirect_to; exit();}
 
 if(array_key_exists("primary_folder",$_GET) && array_key_exists("secondary_folder",$_GET)){
 	if(	$_GET['primary_folder'] == '' 		OR 
@@ -38,7 +38,7 @@ if(array_key_exists("primary_folder",$_GET) && array_key_exists("secondary_folde
 		else{$url_to_redirect_to .= "&";}
 		
 		$url_to_redirect_to .= url_folder_get_string_creation();
-		if($_SERVER['HTTP_HOST']!= "dreamboat.com.au"){echo "url to redirect to:".$url_to_redirect_to; exit();}
+
 
 		header("Location: ".$url_to_redirect_to);
 		exit();
