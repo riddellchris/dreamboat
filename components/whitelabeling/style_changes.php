facc1c324b5ad6a47c5ae95b6f9cb3d2ce35d0e2
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-16 22:34:42 +1100

    Update style_changes.php

--- components/whitelabeling/style_changes.php
+++ components/whitelabeling/style_changes.php
@@ -3,7 +3,23 @@ if(!isset($_SESSION)){session_start();}
 
 if(isset($_SESSION['masked_domain'])){
 
-
+	if($_SESSION['masked_domain'] == 'nashnewton'){					
+                                                                $_SESSION['masked_domain_primary_colour']           = " black ";
+                                                                $_SESSION['masked_domain_primary_colour_darker']    = " #1f731f ";
+                                                                $_SESSION['masked_domain_menu_tone_one']            = " #28b215 ";
+                                                                $_SESSION['masked_domain_menu_tone_two']            = " #1edb04 ";
+                                                                $_SESSION['masked_domain_highlight_colour']         = " #d6fc49 ";
+                                                                $_SESSION['masked_domain_focus_colour']             = " #2b0def ";
+                                                                $_SESSION['masked_domain_logo_image']               = "yes";
+                                                                    $_SESSION['masked_domain_logo_file_name']       = "";                                                 
+                                                                $_SESSION['masked_domain_logo_name']                = "irrth";
+                                                                $_SESSION['masked_domain_logo_font_family']         = "PTSansRegular, Arial, Helvetica, sans-serif";
+                                                                $_SESSION['masked_domain_logo_letter_spacing']      = "0.1em";
+                                                                $_SESSION['masked_domain_logo_color']               = "#d6fc49";
+                                                                $_SESSION['masked_domain_logo_font_weight']         = "700";  
+                                                                $_SESSION['masked_domain_logo_text_transform']      = "none";
+                                                                $_SESSION['masked_domain_html_title']               = "Nash Newton";
+}
 	if($_SESSION['masked_domain'] == 'irrth'){					
                                                                 $_SESSION['masked_domain_primary_colour']           = " forestgreen ";
                                                                 $_SESSION['masked_domain_primary_colour_darker']    = " #1f731f ";
@@ -19,7 +35,7 @@ if(isset($_SESSION['masked_domain'])){
                                                                 $_SESSION['masked_domain_logo_color']               = "#d6fc49";
                                                                 $_SESSION['masked_domain_logo_font_weight']         = "700";  
                                                                 $_SESSION['masked_domain_logo_text_transform']      = "none";
-                                                                $_SESSION['masked_domain_html_title']               = "irrth Solutions";
+                                                                $_SESSION['masked_domain_html_title']               = "irrth.com";
 }
 
 
