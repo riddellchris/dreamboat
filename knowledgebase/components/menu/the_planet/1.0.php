ad474fc86d3c1ac17bc281a9909fa7ab5a2d2cb2
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 19:59:08 +1100

    Update 1.0.php

--- knowledgebase/components/menu/the_planet/1.0.php
+++ knowledgebase/components/menu/the_planet/1.0.php
@@ -2,4 +2,6 @@ m<?php
 if(!isset($_SESSION)){session_start();}
 
 primary_knowledgebase_menu_item('the_planet','no');
-	secondary_knowledgebase_menu_item('current_state','no');	
+if($_GET['secondary_folder'] == 'the_planet'){
+	secondary_knowledgebase_menu_item('current_state','no');
+}
