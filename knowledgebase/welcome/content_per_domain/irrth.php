211881e0ed1d805b9044650863eac49cdcf4b8af
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 18:33:23 +1100

    Create irrth.php

---
+++ knowledgebase/welcome/content_per_domain/irrth.php
@@ -0,0 +1,43 @@
+paragraph_start('yes', 10, 9, 9, "Welcome to Dreamboat's knowledgebase.");
+
+
+paragraph_start('no', 9,8,8, "Here you'll find all publicly available information on how we are stabilitising Australia's economy by optimising the individual <a href='/knowledgebase/productivity/network_productivity/' target='_blank'>productivity networks</a> that the economy is built of.");
+paragraph_start('no', 7,8,8, "A visual representation of that elemental network, is seen below: ");
+
+echo "<br><br><br>";
+// removing issues that limit productivity from Australia's .");
+include_knowledgebase_image('/knowledgebase/components/diagrams/network_issues_101.svg', 'default', '65vh');
+
+echo "<br><br><br>";
+
+
+//$copy = "This network is the <a href='https://en.wikipedia.org/wiki/Finite_element_method' target='_blank'>Finite Element</a> that the economy relies upon & is built of.";	
+//require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/components/notifiers/fact.php";
+
+
+// removing issues that limit productivity from Australia's .");
+paragraph_start('no', 7,8,8, "Combined together, similar to <a href='https://en.wikipedia.org/wiki/Finite_element_method' target='_blank'>Finite Element Analysis</a>, we can see, assess & optimise the economy just like a physical material. This allows us to optimise on a far more granular basis than ever before to provide an additional layer of economic optimisation to the country. ");
+echo "<br><br><br>";
+include_knowledgebase_image('/knowledgebase/components/diagrams/economic_network.svg', 'default', '65vh');
+echo "<br><br><br>";
+
+paragraph_start('no', 8, 8, 9, "As we map each network, track it's performance and optimise it; both by itself and in conjunction with those around it we optimise the economy, stabilise the economy and avoid the <a href='/knowledgebase/economy/disaster/downside_risks' target='_blank'>downside risks</a> surrounding the health of the economy for the longer term too.");
+//include_knowledgebase_image('/knowledgebase/welcome/diagrams/network_issues_202.svg', 'default', '65vh');
+
+//paragraph_start('no', 8, 8, 9, "... we are creating continuous productivity improvement ...");
+//economic/Objective
+//include_knowledgebase_image('/knowledgebase/welcome/diagrams/Final_unassisted_productivity.svg', 'default', '65vh');
+//paragraph_start('no', 8, 8, 9, "...that should ultimately create economic growth and wage growth as some proportion of that productivity growth (as has always happened).");
+//economic/Objective
+//include_knowledgebase_image('/knowledgebase/welcome/diagrams/Objective.svg', 'default', '65vh');
+
+
+echo "<br><br><br>";
+paragraph_detailed('no',10,10,10,"Our hope is that by doing this we can avoid economic <a href='/knowledgebase/economy/problem/' target='_blank'>disaster</a> and:");
+	list_start();
+		point_detailed_start('no',8,8,8,"Create a stable economy for the future");
+		point_detailed_start('no',8,8,8,"Give the economy a third leg to stand on other than the RBA and Government budgets");
+		point_detailed_start('no',8,8,8,"Enable productivity improvement across the country");
+		point_detailed_start('no',8,8,8,"Enable full employment");
+		point_detailed_start('no',8,8,8,"Ultimately enable exports of our new activities");
+	list_end();
