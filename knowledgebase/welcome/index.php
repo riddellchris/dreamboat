5995f0692835a61195dce33638463f302d3e5d14
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 18:43:10 +1100

    Update index.php

--- knowledgebase/welcome/index.php
+++ knowledgebase/welcome/index.php
@@ -2,8 +2,8 @@
 if(!isset($_SESSION)){session_start();}
 require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/components/layout/pre_content.php";
 
-if($_SESSION['HTTP_HOST'] == "dreamboat.com.au"){	require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/dreamboat.php";}
-if($_SESSION['HTTP_HOST'] == "irrth.com"){		require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/irrth.php";}
-if($_SESSION['HTTP_HOST'] == "nashnewton.com.au"){	require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/nashnewton.php";}
+if($_SERVER['HTTP_HOST'] == "dreamboat.com.au"){	require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/dreamboat.php";}
+if($_SERVER['HTTP_HOST'] == "irrth.com"){		require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/irrth.php";}
+if($_SERVER['HTTP_HOST'] == "nashnewton.com.au"){	require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/nashnewton.php";}
 
 require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/components/layout/post_content.php";
