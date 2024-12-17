e0dd83a91ad6aed556991b627d06d28c534c6c92
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 19:16:47 +1100

    Update 1.0.php

--- knowledgebase/components/menu/the_planet/1.0.php
+++ knowledgebase/components/menu/the_planet/1.0.php
@@ -3,11 +3,7 @@ if(!isset($_SESSION)){session_start();}
 $current_version = "1.0";
 require dirname(__FILE__)."/".basename(__FILE__, ".php")."/".$current_version.".php";
 
-
 /*
-
 1.0
 Just the basic standard stuff
-
-
 */
