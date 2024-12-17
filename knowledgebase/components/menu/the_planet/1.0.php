e705758348181bcc682ec778601e6c70efe8bcfa
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 19:27:03 +1100

    Update 1.0.php

--- knowledgebase/components/menu/the_planet/1.0.php
+++ knowledgebase/components/menu/the_planet/1.0.php
@@ -1,9 +1,5 @@
 <?php
 if(!isset($_SESSION)){session_start();}
-$current_version = "1.0";
-require dirname(__FILE__)."/".basename(__FILE__, ".php")."/".$current_version.".php";
 
-/*
-1.0
-Just the basic standard stuff
-*/
+
+primary_knowledgebase_menu_item('the_planet','no');
