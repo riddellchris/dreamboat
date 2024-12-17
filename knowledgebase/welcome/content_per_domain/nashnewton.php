16eaeaaab1342f7a377ba6a3da5927549a76b72c
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 18:51:04 +1100

    Update nashnewton.php

--- knowledgebase/welcome/content_per_domain/nashnewton.php
+++ knowledgebase/welcome/content_per_domain/nashnewton.php
@@ -1,20 +1,8 @@
 <?php
 
-paragraph_start('yes', 10, 9, 9, "Welcome to Dreamboat's knowledgebase.");
+paragraph_start('yes', 10, 9, 9, "Hi & welcome to Nash Newton named after two of the greatest minds we know <a href='https://en.wikipedia.org/wiki/John_Forbes_Nash_Jr.' target='_blank'>John Nash</a> and obviously <a href='https://en.wikipedia.org/wiki/Isaac_Newton' target='_blank'>Sir Isaac Newton</a> where here at Nash Newton we are not only inspired but attempt to use intelligence to think cleanly and clearly to not only reshape games but also solve problems via clever investment.");
 
-
-paragraph_start('no', 9,8,8, "Here you'll find all publicly available information on how we are stabilitising Australia's economy by optimising the individual <a href='/knowledgebase/productivity/network_productivity/' target='_blank'>productivity networks</a> that the economy is built of.");
-paragraph_start('no', 7,8,8, "A visual representation of that elemental network, is seen below: ");
-
-echo "<br><br><br>";
-// removing issues that limit productivity from Australia's .");
-include_knowledgebase_image('/knowledgebase/components/diagrams/network_issues_101.svg', 'default', '65vh');
-
-echo "<br><br><br>";
-
-
-//$copy = "This network is the <a href='https://en.wikipedia.org/wiki/Finite_element_method' target='_blank'>Finite Element</a> that the economy relies upon & is built of.";	
-//require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/components/notifiers/fact.php";
+paragraph_start('yes', 10, 9, 9, "It would be a stretch to say that we intend to think differently just for the sake of it, but we do like ask ourselves, now why is this perspective incorrect and as a result find unique perspectives that open up new and interesting opportunities for investment.");
 
 
 // removing issues that limit productivity from Australia's .");
