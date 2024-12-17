fd43cbf1f1e8ce17c64f6cc69b46e40734c079e2
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 18:57:29 +1100

    Update nashnewton.php

--- knowledgebase/welcome/content_per_domain/nashnewton.php
+++ knowledgebase/welcome/content_per_domain/nashnewton.php
@@ -1,8 +1,8 @@
 <?php
 
-paragraph_start('yes', 10, 9, 9, "Hi & welcome to Nash Newton named after two of the greatest minds we know <a href='https://en.wikipedia.org/wiki/John_Forbes_Nash_Jr.' target='_blank'>John Nash</a> and obviously <a href='https://en.wikipedia.org/wiki/Isaac_Newton' target='_blank'>Sir Isaac Newton</a> where here at Nash Newton we are not only inspired but attempt to use intelligence to think cleanly and clearly to not only reshape games but also solve problems via clever investment.");
+paragraph_start('no', 10, 9, 9, "Hi & welcome to Nash Newton named after two of the greatest minds we know <a href='https://en.wikipedia.org/wiki/John_Forbes_Nash_Jr.' target='_blank'>John Nash</a> and obviously <a href='https://en.wikipedia.org/wiki/Isaac_Newton' target='_blank'>Sir Isaac Newton</a> where here at Nash Newton we are not only inspired but attempt to use intelligence to think cleanly and clearly to not only reshape games but also solve problems via clever investment.");
 
-paragraph_start('yes', 10, 9, 9, "It would be a stretch to say that we intend to think differently just for the sake of it, but we do like ask ourselves, now why is this perspective incorrect and as a result find unique perspectives that open up new and interesting opportunities for investment.");
+paragraph_start('no', 10, 9, 9, "It would be a stretch to say that we intend to think differently just for the sake of it, but we do like ask ourselves, now why is this perspective incorrect and as a result find unique perspectives that open up new and interesting opportunities for investment.");
 
 
 // removing issues that limit productivity from Australia's .");
