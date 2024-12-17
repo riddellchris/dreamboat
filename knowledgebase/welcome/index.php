921462c9c9d7d32b9e4f86bb888673b52dc27fcc
riddellchris <90325215+riddellchris@users.noreply.github.com>
2024-12-17 18:37:00 +1100

    Update index.php

--- knowledgebase/welcome/index.php
+++ knowledgebase/welcome/index.php
@@ -3,6 +3,6 @@ if(!isset($_SESSION)){session_start();}
 require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/components/layout/pre_content.php";
 
 if($_SESSION['HTTP_HOST'] == "dreamboat.com.au"){require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/dreamboat.php"}
-if($_SESSION['HTTP_HOST'] == "irrth.com.au"){require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/irrth.php"}
+if($_SESSION['HTTP_HOST'] == "irrth.com"){require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/welcome/content_per_domain/irrth.php"}
 	
 require $_SERVER['DOCUMENT_ROOT']."/knowledgebase/components/layout/post_content.php";
