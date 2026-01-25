1bad28ebb523dde9d97bbadf67dc12e00372c89b
riddellchris <90325215+riddellchris@users.noreply.github.com>
2026-01-25 20:34:27 +1100

    The whole process is resolved
    Added initial comments outlining the business process and challenges.

---
+++ knowledgebase/processes/summary/processes.php
@@ -0,0 +1,7 @@
+//Clearly what's been amazing here and honestly is amazing is that I have actually solved this entire thing now from head to toe.
+//That much is clearly true.
+
+// So the reality is that: yes, ultimatley this is a book keeping company which is amazing and that positioning is amazing but the thing that I need to know and focus on is that:
+//A - Have managed to build a way to engage the market that flows all the way through to transformation plans that then flows all the way through to data, bookkeeping and all the ongoing work as well.
+//B - The thing that isn't ideal about this process and this problem though that actually none of it is going through the critical platform yet and
+  // I'm probably paying way over the odds for things that I don't really need now and profitability is actually so incredibly close now because I can see it happening for sure.
