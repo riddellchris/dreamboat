5a8c2296215120a116b7fb9969969fb890f5f906
riddellchris <90325215+riddellchris@users.noreply.github.com>
2026-02-21 17:01:50 +1100

    alignment v9

--- overview/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!isset($_SESSION)){session_start();}
-require $_SERVER['DOCUMENT_ROOT']."/components/functions/url_and_folder_functions.php";
-header("Location: ".primary_folders_url().$_SESSION[primary_folders_name().'_page']."/");
-
